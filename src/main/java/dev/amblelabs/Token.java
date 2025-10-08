c41929294d7e5fa35dd9e1ac100d3c7e4c8e56cb
Loqor <isaiahcrace@gmail.com>
2025-10-07 23:25:59 -0500

    A Grammar for Lox expressions - Chapter 5.1.3
    (This is where I'm at in the book)

---
+++ src/main/java/dev/amblelabs/Token.java
@@ -0,0 +1,8 @@
+package dev.amblelabs;
+
+public record Token(TokenType type, String lexeme, Object literal, int line) {
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
---
+++ src/main/java/dev/amblelabs/TokenType.java
@@ -0,0 +1,22 @@
+package dev.amblelabs;
+
+public enum TokenType {
+    // Single Character
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two characters
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords - we'll prolly use && and || and whatnot
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
